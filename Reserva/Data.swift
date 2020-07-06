0b5f0f54881f6387b5c3eca4591ca26230005e7b
Granwyn Tan <->
2020-07-06 21:34:40 +0800

    Function to set properties for button

--- Reserva/Data.swift
+++ Reserva/Data.swift
@@ -14,3 +14,9 @@ var color2 = UIColor(red: 60/255, green: 60/255, blue: 60/255, alpha: 1.0)
 var color3 = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
 
 var cornerRadius1 = 5
+
+func setPropertiesOf(_ buttonName: UIButton, _ buttonCornerRadiusValue: CGFloat, _ buttonBext: String, _ buttonBackgroundColor: UIColor){
+    buttonName.layer.cornerRadius = buttonCornerRadiusValue
+    buttonName.setTitle(buttonText, for: .normal)
+    buttonName.backgroundColor = buttonBackgroundColor
+}
