aac7571aeb591f5355fa8fa5f627f3da30d0e980
hojonyew <ho_jon_yew@s2019.ssts.edu.sg>
2020-07-06 21:13:51 +0800

    Applied UIColors, corner radius and background colors to labels and buttons through global variables

--- Reserva/Data.swift
+++ Reserva/Data.swift
@@ -11,4 +11,6 @@ import UIKit
 
 var color1 = UIColor(red: 114/255, green: 147/255, blue: 255/255, alpha: 1.0)
 var color2 = UIColor(red: 60/255, green: 60/255, blue: 60/255, alpha: 1.0)
+var color3 = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
 
+var cornerRadius1 = 5
--- Reserva/WelcomeViewController.swift
+++ Reserva/WelcomeViewController.swift
@@ -12,10 +12,19 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
-
+    
+    @IBOutlet weak var getStartedButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
+    
     func UIsetup () {
         titleLabel.textColor = color1
         subtitleLabel.textColor = color2
+        
+        getStartedButton.layer.cornerRadius = CGFloat(cornerRadius1)
+        getStartedButton.backgroundColor = color1
+        getStartedButton.setTitleColor(color3, for: .normal)
+        
+        signInButton.setTitleColor(color1, for: .normal)
     }
     
     override func viewDidLoad() {
