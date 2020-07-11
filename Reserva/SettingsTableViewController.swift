e9bed51316909ada166d3572199265666dc6424a
Granwyn Tan <->
2020-07-11 22:49:29 +0800

    Bug Fixes

--- Reserva/SettingsTableViewController.swift
+++ Reserva/SettingsTableViewController.swift
@@ -46,7 +46,7 @@ class SettingsTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         // Configure the cell...
         var cellMainText = ""
