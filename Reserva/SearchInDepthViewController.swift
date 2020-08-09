85ce5f45d86841e96d780a3ccf5e566483ad7678
Joeispro99 <65899074+Joeispro99@users.noreply.github.com>
2020-08-09 20:03:46 +0800

    i keep having errors cos i not smart

--- Reserva/SearchInDepthViewController.swift
+++ Reserva/SearchInDepthViewController.swift
@@ -11,13 +11,45 @@ import UIKit
 
 class SearchInDepthViewController: UIViewController {
     
+    @IBOutlet weak var bookButton: UIButton!
+    @IBOutlet weak var wishlistButton: UIButton!
+    @IBOutlet weak var followButton: UIButton!
+    @IBOutlet weak var inDepthDateLabel: UILabel!
+    @IBOutlet weak var inDepthWebsiteLabel: UILabel!
+    @IBOutlet weak var inDepthReviewLabel: UILabel!
+    @IBOutlet weak var inDepthAdmission: UILabel!
+    @IBOutlet weak var inDepthEventDescription: UILabel!
+    @IBOutlet weak var inDepthImageView: UIImageView!
+    @IBOutlet weak var inDepthEventName: UILabel!
+    
     var stuff: Event?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        bookButton.layer.cornerRadius = 20
+        followButton.layer.cornerRadius = 20
+        wishlistButton.layer.cornerRadius = 20
+        
+        inDepthImageView.image = UIImage(named: stuff!.eventImage)
+        inDepthEventName.text = stuff!.eventName
+        inDepthEventDescription.text = stuff!.eventDescription
+        inDepthAdmission.text = stuff!.eventCost
+        inDepthDateLabel.text = "\(String(describing: stuff!.eventStartDate)) - \(String(describing: stuff!.eventEndDate))"
+        inDepthReviewLabel.text = ""
+        inDepthWebsiteLabel.text = ""
     }
+    
+    @IBAction func bookPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "You have booked for \(stuff!.eventName)", message: "Check your events for the events you signed up for!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
     
 
     /*
