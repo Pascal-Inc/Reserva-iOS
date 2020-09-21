82a1a13d3ffb31a1867079e3f96bede60618ffb9
Joeispro99 <65899074+Joeispro99@users.noreply.github.com>
2020-09-21 20:04:02 +0800

    Cloud Firestore

--- Reserva/AppDelegate.swift
+++ Reserva/AppDelegate.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
-//import Firebase
+import Firebase
+import FirebaseFirestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //FirebaseApp.configure()
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
         return true
     }
 
